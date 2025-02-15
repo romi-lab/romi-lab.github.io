8849e25c5455f40463843f7fe4000fbbb422de17
samanthalhy <70717755+samanthalhy@users.noreply.github.com>
2025-02-15 21:30:28 +0800

    minor update

--- simulation/advanced/js/code.js
+++ simulation/advanced/js/code.js
@@ -5,3 +5,18 @@ $(document).ready(function () {
     });
 
 });
+window.setInterval(function() {
+
+  var current = new Date();
+  var expiry = new Date("February 15, 2025 21:30:00")
+
+  if (current.getTime() < expiry.getTime()) {
+    $('.questions').hide();
+    $('.holdon').show();
+
+  } else if (current.getTime() > expiry.getTime()) {
+    $('.questions').show();
+    $('.holdon').hide();
+  }
+
+}, 0);
