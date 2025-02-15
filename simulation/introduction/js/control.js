b8f2b726d2910819de59a3a24c7e44705bbeff30
samanthalhy <70717755+samanthalhy@users.noreply.github.com>
2025-02-15 22:21:52 +0800

    Update control.js

--- simulation/introduction/js/control.js
+++ simulation/introduction/js/control.js
@@ -1,7 +1,7 @@
 window.setInterval(function() {
 
   var current = new Date();
-  var expiry = new Date("March 21, 2025 14:59:00")
+  var expiry = new Date("March 7, 2025 14:59:00")
 
   if (current.getTime() < expiry.getTime()) {
     $('.questions').hide();
