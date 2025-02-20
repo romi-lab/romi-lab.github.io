7ac08ef792838adeafe4fc88c609eb208a57c32e
ning-han99 <ningg.han@connect.polyu.hk>
2025-02-20 18:01:14 +0800

    Update control.js

--- simulation/introduction/js/control.js
+++ simulation/introduction/js/control.js
@@ -1,7 +1,7 @@
 window.setInterval(function() {
 
   var current = new Date();
-  var expiry = new Date("March 7, 2024 14:59:00")
+  var expiry = new Date("February 21, 2025 14:59:00")
 
   if (current.getTime() < expiry.getTime()) {
     $('.questions').hide();
